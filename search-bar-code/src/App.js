8fbe86c5a1eecf1ca537b1cadd7fd7958467c637
alan190902 <alan.kouakou@edu.esiee-it.fr>
2022-06-18 18:34:24 +0200

    ✨ - fetch data in react with a custom usefetch hook

--- search-bar-code/src/App.js
+++ search-bar-code/src/App.js
@@ -5,12 +5,24 @@ import Footer from "./Components/Footer";
 import Home from "./pages/Home";
 import SearchBar from "./Components/SearchBar";
 import BookData from "./Data.json";
+import useFetch from "./Components/useFetch";
 
 function App() {
+
+  const { data: joke, loading, error, refetch } = useFetch(
+    "https://v2.jokeapi.dev/joke/Any"
+  );
+
+  if (loading) return <h1> LOADING...</h1>;
+
+  if (error) console.log(error);
+  
   return (
     <div className="App">
       <Navbar />
       <SearchBar placeholder="Enter a Book Name..." data={BookData} />
+      <h1> {joke?.setup} : {joke?.delivery} </h1>
+      <button onClick={refetch}> Refetch</button>
       <Home />
       <Footer />
     </div>
