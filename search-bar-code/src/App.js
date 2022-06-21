9229a7b94c2905806ea4fe92ddec83910f6ae3a4
alan190902 <alan.kouakou@edu.esiee-it.fr>
2022-06-21 09:18:26 +0200

    ✨ - searchbar updated

--- search-bar-code/src/App.js
+++ search-bar-code/src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+/* import React from "react";
 import "./App.css";
 import Navbar from "./Components/Navbar";
 import Footer from "./Components/Footer";
@@ -22,9 +22,60 @@ function App() {
       <Navbar />
       <SearchBar placeholder="Enter a Book Name..." data={BookData} />
       <h1> {joke?.setup} : {joke?.delivery} </h1>
-      <button onClick={refetch}> Refetch</button>
       <Home />
       <Footer />
+    </div>
+  );
+}
+
+export default App; */
+
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import axios from "axios";
+
+function App() {
+  const [loading, setLoading] = useState(false);
+  const [posts, setPosts] = useState([]);
+  const [searchTitle, setSearchTitle] = useState("");
+
+  useEffect(() => {
+    const loadPosts = async () => {
+      setLoading(true);
+      const response = await axios.get(
+        "http://localhost:8000/search/"
+      );
+      setPosts(response.data);
+      setLoading(false);
+    };
+
+    loadPosts();
+  }, []);
+
+  return (
+    <div className="App">
+      <h3>Search Filter</h3>
+      <input
+        style={{ width: "30%", height: "25px" }}
+        type="text"
+        placeholder="Search..."
+        onChange={(e) => setSearchTitle(e.target.value)}
+      />
+      {loading ? (
+        <h4>Loading ...</h4>
+      ) : (
+        posts
+          .filter((value) => {
+            if (searchTitle === "") {
+              return value;
+            } else if (
+              value.name.toLowerCase().includes(searchTitle.toLowerCase())
+            ) {
+              return value;
+            }
+          })
+          .map((item) => <h5 key={item.id}>{item.name}</h5>)
+      )}
     </div>
   );
 }
